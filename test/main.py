795202be92767b4f665322c3e83ab8ad95106180
David <uched41@yahoo.com>
2019-07-11 12:41:31 +0300

    Adding threading to tracking class

--- test/main.py
+++ test/main.py
@@ -1,5 +1,7 @@
 from object_tracker import Tracker
 
 tracker = Tracker("rtsp://10.90.131.159:554/ch0_0.h264", "CSRT")
-tracker.track()
+tracker.start()
 
+while 1:
+	print(tracker.target[0], tracker.target[1])
--- test/object_tracker.py
+++ test/object_tracker.py
@@ -5,16 +5,19 @@ CSRT object tracker is used
 """
 import cv2 as cv
 import sys
+import threading
 
-class Tracker(object):
+class Tracker(threading.Thread):
 	def __init__(self, ipAddress, trackerType):
+		threading.Thread.__init__(self)
 		self.ipAddress = ipAddress
 		self.trackerType = trackerType
-	
+		self.target = [0, 0, 0, 0]
+
 	def createTracker(self):
 		tracker = cv.TrackerCSRT_create()
 		return tracker
-	
+
 	def openVideoStream(self):
 		video = cv.VideoCapture(self.ipAddress)
 		if not video.isOpened():
@@ -26,35 +29,34 @@ class Tracker(object):
 	def selectROI(self, frame):
 		bbox = cv.selectROI(frame, False)
 		return bbox
-	
-	def track(self):
+
+	def run(self):
 		tracker = self.createTracker()
 		video = self.openVideoStream()
 		ok, frame = video.read()
 		if not ok:
 			print ("Can not read a video stream")
 			sys.exit(-1)
-		target = self.selectROI(frame)
-		ok = tracker.init(frame, target)
+		self.target = self.selectROI(frame)
+		ok = tracker.init(frame, self.target)
 		cv.destroyAllWindows()
 
 		while True:
 			ok, frame = video.read()
 			if not ok:
 				break
-			ok, target = tracker.update(frame)
+			ok, self.target = tracker.update(frame)
 			if ok:
-				p1 = (int(target[0]), int(target[1]))
-				p2 = (int(target[0]+target[2]), int(target[1]+target[3]))
+				p1 = (int(self.target[0]), int(self.target[1]))
+				p2 = (int(self.target[0]+self.target[2]), int(self.target[1]+self.target[3]))
 				cv.rectangle(frame, p1, p2, (0, 0, 255), 2, 1)
 			else:
 				cv.putText(frame, "Tracking failed", (100,80), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0,0,255), 2)
-			cv.imshow("frame", frame)
-			
+
+			cv.imshow("Frame", frame)
+
 			if cv.waitKey(1) & 0xFF == ord('q'):
 				video.release()
 				cv.destroyAllWindows()
 				break
 
-		
-		
